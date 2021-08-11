0b1e9c9a1c9c70e1ff3e5919bc0407c9ba7af717
Naveen <64829124+naveen509@users.noreply.github.com>
2021-08-11 07:49:35 +0530

    Add files via upload
    initial commit

---
+++ supplement_Shop/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Landing Page</title>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<script src="js/jquery_js.js"> </script>
+
+</head>
+
+
+
+<body>
+<meta name="viewport" content="width=device=width,initial-scale=1.0">
+	<div class="container">
+<div style="background-image: url('img/virat.jpg');">
+
+  		<header>
+
+  			<div style="background-image: url('img/virat.jpg');">
+		<h1>Bulked Up Boost</h1>
+		<h2>"Last Rep Do Matters..! "</h2>
+		<a class="btn">Order Now</a>
+
+<!--This is a comment. Comments are not displayed in the browser
+		<div class="submenu">
+			<a href="#">I am a begginer -></a>
+			<a href="#">I am an Intermidiate  -></a>
+			
+
+		</div>
+-->
+			
+  		</header>
+
+  	</div>
+
+
+  <section class="features">
+<div style="background-image: url('img/virat.jpg');">
+
+  	<div class="container">
+  		
+  			<div class="feature">
+			<img src="img/creatine.jpg">
+  			<p> Protien</p>
+  			
+
+
+  	</div>
+
+  	<div class="feature">
+  		<img src="img/prework.jpg">
+  		<p> Pre-Workouts</p>
+  			
+
+
+
+
+  	</div>
+
+  	<div class="feature">
+  		<img src="img/creatine.jpg">
+  		<p> Creatine</p>
+  			
+
+
+
+
+  	</div>
+
+
+  	<div class="feature">
+  		<img src="img/mass.jpg">
+  		<p> Mass Gainers</p>
+  			
+
+
+
+
+  	</div>
+
+  	<div class="feature">
+  		<img src="img/fatburn.jpg">
+  		<p> Fat Burners</p>
+  			
+
+
+
+
+  	</div>
+
+  	<div class="feature">
+  		<img src="img/recover.jpg">
+  		<p> Recovery</p>
+  			
+
+
+
+
+  	</div>
+
+
+
+
+  	</div>
+
+
+  	
+  </section>
+
+  <section class="quote">
+
+  	<blockquote class="container">
+  	<p> "Last Rep Do Matters!!"</p>
+  	<cite>Satisfied Customer</cite>	
+
+  	</blockquote>
+
+
+
+  	
+  </section>
+
+  <footer>
+
+  	<div class="container">
+  		<ul>
+  			<li><a href="#">Home</a></li>
+  			<li><a href="#">About</a></li>
+  			<li><a href="#">Contact us</a></li>
+  		</ul>
+  		
+  			<p>All rights reserved.</p>
+
+  	</div>
+
+  	
+
+  </footer>
+
+
+
+</body>
+</html>
---
+++ supplement_Shop/js/jquery_js.js
@@ -0,0 +1,9 @@
+$(function() {// jquery start
+	$("btn").click(function(){
+
+		$(".submenu").slideToggle(500);
+
+
+	});// btn click event closed
+
+});// jquery closed
